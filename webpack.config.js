b3bf25b6d6b409c92a43a0ef1165436983585786
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-04-09 12:37:12 +0530

    Added index.css in src and moved all other css inside styles directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,37 @@
 const webpack = require("webpack");
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
-  entry: "./src/index.css",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.css",
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png",
+            },
+          },
+        ],
       },
     ],
   },
+  plugins: [new MiniCssExtractPlugin()],
 };
 
 module.exports = config;
